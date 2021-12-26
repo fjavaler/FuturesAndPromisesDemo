8c22503618a059fd7495dc8111ba28732f5f594b
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-26 14:24:21 -0700

    Initial Commit

---
+++ FuturesAndPromises/FuturesAndPromisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuturesAndPromisesApp.swift
+//  FuturesAndPromises
+//
+//  Created by Frederick Javalera on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct FuturesAndPromisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
